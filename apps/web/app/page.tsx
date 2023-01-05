43146d07640875cbed89b898d4860cc273228c0c
Md Irshad <mdirshaddev+github@gmail.com>
2023-01-05 09:11:23 +0530

    refractor: 🏗️ refractoring some small chunks of web app code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,4 @@
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
 import { Introduction } from 'src/feature/landing/components/Introduction';
-// import Overlay from 'src/components/Overlay';
-import Box from 'src/shared/Box';
-import Container from 'src/shared/Container';
 import SectionWithParallax from 'src/shared/SectionWithParallax';
 
 export default async function Page() {
@@ -21,98 +16,7 @@ export default async function Page() {
           <Introduction />
         </div>
         <div className='h-0 lg:h-[105vh]' />
-        <SectionWithParallax>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-            explicabo perferendis voluptates cupiditate mollitia, esse itaque
-            vero odio necessitatibus. Molestiae est dignissimos itaque minus
-            explicabo! Aut corporis expedita a delectus?
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-          <div className='text-black dark:text-white'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-            molestiae, eaque aliquam vitae repellendus natus consequatur
-            assumenda ex ducimus et est ipsa perferendis error beatae illo
-            repudiandae eos aperiam quam.
-          </div>
-        </SectionWithParallax>
+        <SectionWithParallax></SectionWithParallax>
       </main>
     </>
   );

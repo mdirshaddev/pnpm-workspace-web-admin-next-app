85c16091fd1995da0027d5df290c021284818b9f
Md Irshad <mdirshaddev+github@gmail.com>
2023-01-25 00:01:08 +0530

    storybook: 🛠️ config updated
    More integrations and some preview features are in use for better development.

--- apps/web/.storybook/main.js
+++ apps/web/.storybook/main.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const TsConfigWebpackPath = require('tsconfig-paths-webpack-plugin');
+const TsConfigWebpackPath = require('tsconfig-paths-webpack-plugin'); // Link typescript path config with webpack config
 
 const storybookConfig = {
   staticDirs: ['../public'],
@@ -11,9 +11,11 @@ const storybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-swc',
+    '@storybook/addon-a11y', // For Accessibility warning and suggestions
+    'storybook-addon-swc', // For SWC Compilation
+    'storybook-addon-pseudo-states', // For pseudo state selection
     {
-      name: 'storybook-addon-next',
+      name: 'storybook-addon-next', // For Nextjs capabilities
       options: {
         nextConfigPath: path.resolve(__dirname, '../next.config.js')
       }
@@ -24,9 +26,9 @@ const storybookConfig = {
     builder: '@storybook/builder-webpack5'
   },
   features: {
-    previewMdx2: true,
-    postcss: true,
-    storyStoreV7: false
+    previewMdx2: true, // Moving to MDX2 which is fast and have new features
+    postcss: true, // postcss for handling tailwindcss
+    storyStoreV7: true // New fast and on demand loading of storybook which makes startup faster
   },
   webpackFinal: async config => {
     config?.resolve?.plugins?.push(new TsConfigWebpackPath());
